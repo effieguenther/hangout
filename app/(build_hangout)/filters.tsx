1033890d3a628d4f7a2d5e1b43c5854e46e2e7fa
Effie Guenther <effiegguenther@gmail.com>
2025-06-01 20:59:08 -0400

    filters screen skeleton

--- app/(build_hangout)/filters.tsx
+++ app/(build_hangout)/filters.tsx
@@ -1,17 +1,64 @@
 // app/(app)/preferences.js
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Text, useTheme } from 'react-native-paper';
 import BuildHangoutNavigator from '@/components/BuildHangoutNavigator';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { List, Text, useTheme } from 'react-native-paper';
 
 export default function FiltersScreen() {
   const theme = useTheme();
+  const activityFilters = ['DINNER', 'LUNCH', 'BRUNCH', 'COFFEE', 'PARK', 'DRINKS', 'MY PLACE'];
+  const distanceFilters = ['UP TO 15 MINS AWAY', 'UP TO 30 MINS AWAY', 'UP TO 60 MINS AWAY', 'FIND A MIDPOINT'];
+  const budgetFilters = ["$", "$$", "$$$"]
+
   return (
     <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
       <BuildHangoutNavigator currentScreen='/(build_hangout)/filters' />
-      <Text variant="headlineMedium" style={{ color: theme.colors.onBackground }}>
-        Filters Screen
+      <Text variant="bodyMedium" style={{ color: theme.colors.onBackground, marginBottom: 20 }}>
+        OPTIONAL FILTERS
       </Text>
+      <View style={{width: '90%'}}>
+        <List.AccordionGroup>
+          <List.Accordion title="ACTIVITY" id="1" style={styles.accordion}>
+            <View style={styles.listItemContainer}>
+              {
+                activityFilters.map((activity, idx) => (
+                  <List.Item 
+                    key={`activity_item_${idx}`} 
+                    title={activity} 
+                    style={{...styles.listItem, width: '48%'}}
+                  />
+                ))
+              }
+            </View>
+          </List.Accordion>
+          <List.Accordion title="DISTANCE" id="2" style={styles.accordion}>
+            <View style={styles.listItemContainer}>
+              {
+                distanceFilters.map((activity, idx) => (
+                  <List.Item 
+                    key={`distance_item_${idx}`} 
+                    title={activity} 
+                    style={{...styles.listItem, width: '100%'}}
+                  />
+                ))
+              }
+            </View>
+          </List.Accordion>
+          <List.Accordion title="BUDGET" id="3" style={styles.accordion}>
+            <View style={styles.listItemContainer}>
+              {
+                budgetFilters.map((activity, idx) => (
+                  <List.Item 
+                    key={`budget_item_${idx}`} 
+                    title={activity} 
+                    style={{...styles.listItem, width: '100%'}}
+                  />
+                ))
+              }
+            </View>
+          </List.Accordion>
+        </List.AccordionGroup>
+      </View>
     </View>
   );
 }
@@ -19,8 +66,24 @@ export default function FiltersScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     padding: 20,
   },
+  accordion: {
+    borderWidth: 1,
+    borderRadius: 10,
+    marginBottom: 20
+  },
+  listItemContainer: {
+    gap: 10,
+    marginBottom: 20,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+  listItem: {
+    borderWidth: 1,
+    borderRadius: 10
+  }
 });
